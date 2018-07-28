543b6471ec3659adc75b3a3c6ccd1bcdb9bded2a
Luke Frake <lukefrake@gmail.com>
2018-07-28 18:29:25 +0100

    Remove undefined class variables

--- src/components/Stacker/Stacker.js
+++ src/components/Stacker/Stacker.js
@@ -3,9 +3,15 @@ import './Stacker.css';
 import Cards from '../Cards/Cards';
 
 class Stacker extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      classes: this.props.behaviour || "",
+    };
+  }
   render() {
     return (
-      <div className={`Stacker Stacker-${this.props.behaviour}`}>
+      <div className={`Stacker Stacker-${this.state.classes}`}>
         <Cards behaviour={this.props.behaviour}/>
       </div>
     );
